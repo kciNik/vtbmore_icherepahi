a7aa9039556835cba8974be2e8ca7de852e09fac
Шарафутдинов Ренат Русланович <rrsharafutdinov@vtb.ru>
2022-10-08 14:45:41 +0300

    Changed: send auth json

--- server/models/token-model.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model} = require('pg');
-
-const token_schema = new Schema({
-    user: {type: Schema.Types.OblectId, ref: 'User'},
-    refresh_token: {type: String, required: true},
-})
-
-module.exports = model('Token', token_schema);
-
